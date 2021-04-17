3d747897df7d5a5dc405f0633a76eaaeb2f0f434
jin3 <blood.of.cross@icloud.com>
2021-04-17 10:26:01 +0900

    rubocop修正

--- 04.bowling/bowling.rb
+++ 04.bowling/bowling.rb
@@ -1,4 +1,5 @@
 #!/usr/bin/env ruby
+# frozen_string_literal: true
 
 score = ARGV[0]
 scores = score.split(',')
@@ -21,14 +22,12 @@ scores.each do |s|
       frame_count += 1
     end
   else
-    if s == 'X'
-      frame << 10
-    else
-      frame << s.to_i
-    end
-    if scores.length == throw_count
-      frames << frame
-    end
+    frame << if s == 'X'
+               10
+             else
+               s.to_i
+             end
+    frames << frame if scores.length == throw_count
   end
   throw_count += 1
 end
@@ -38,19 +37,19 @@ frame_number = 0
 max_frame = 9
 frames.each do |f|
   if frame_number < 9
-    if f[0] == 10 && frames[frame_number + 1][0] == 10
-      if frame_number == 8
-        point += f[0] + frames[frame_number + 1][0] + frames[frame_number + 1][1]
-      else
-      point += f[0] + frames[frame_number + 1][0] + frames[frame_number + 2][0]
-      end
-    elsif f[0] == 10
-      point += f[0] + frames[frame_number + 1][0] + frames[frame_number + 1][1]
-    elsif f.sum == 10
-      point += f.sum + frames[frame_number + 1][0]
-    else
-      point += f.sum
-    end
+    point += if frame[0] == 10 && frames[frame_number + 1][0] == 10
+               if frame_number == 8
+                 f[0] + frames[frame_number + 1][0] + frames[frame_number + 1][1]
+               else
+                 f[0] + frames[frame_number + 1][0] + frames[frame_number + 2][0]
+               end
+             elsif f[0] == 10
+               10 + frames[frame_number + 1][0] + frames[frame_number + 1][1]
+             elsif f.sum == 10
+               f.sum + frames[frame_number + 1][0]
+             else
+               f.sum
+             end
   elsif frame_number == max_frame
     point += f.sum
   end
